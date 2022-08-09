8a012d872af5c407ad9ab3fe84a0daed127cb3ec
Skg4541 <68281074+Skg4541@users.noreply.github.com>
2022-08-09 13:14:32 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# File Organiser
+
+This tool can be used for organising the files and generating tree of a directory. We all have unorganised directories and the best example of that in everyone's system is "Downloads" directory. So,if you deploy this project to your system and run the command then it will work for you and give you different directory for different file types like all videos will be in one directory, all documents will be in a directory etc. and you will get a well organised directory.
+
+## Authors
+
+- [@PrakashSarangi](https://github.com/PrakashSarangi)
+
+
+## Features
+
+- Organise files of a directory.
+- View tree of a directory.
+- Cross platform.
+
+
+## 🛠 Skills used in this project
+Javascript, Node.js
+
---
+++ main.js
@@ -0,0 +1,172 @@
+#!/usr/bin/env node
+let inputArr = process.argv.slice(2);
+let fs = require("fs");
+let path = require("path");
+
+
+
+// console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organize "directoryPath"
+// node main.js help
+let command = inputArr[0];
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+switch (command) {
+    case "tree":
+        treeFn(inputArr[1]);
+        break;
+    case "organize":
+        organizeFn(inputArr[1]);
+        break;
+    case "help":
+        helpFn();
+        break;
+    default:
+        console.log("Please 🙏 Input Right command");
+        break;
+}
+
+
+function treeFn(dirPath) {
+    // let destPath;
+    let fs = require("fs");
+    if (dirPath == undefined) {
+
+        treeHelper(process.cwd(), "");
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            treeHelper(dirPath, "");
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath, indent) {
+    // is file or folder
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if (isFile == true) {
+        let fileName = path.basename(dirPath);
+        console.log(indent + "├──" + fileName);
+    } else {
+        let dirName = path.basename(dirPath)
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for (let i = 0; i < childrens.length; i++) {
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath, indent + "\t");
+        }
+    }
+
+
+}
+
+
+
+
+function organizeFn(dirPath) {
+    // console.log("organize command implemnted for ", dirPath);
+    // 1. input -> directory path given
+    let destPath;
+    if (dirPath == undefined) {
+        destPath = process.cwd();
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+
+            // 2. create -> organized_files -> directory
+            destPath = path.join(dirPath, "organized_files");
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);
+            }
+
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+    organizeHelper(dirPath, destPath);
+    // 3. identify categories of all the files present in that input directory  ->
+}
+function organizeHelper(src, dest) {
+    // 3. identify categories of all the files present in that input directory  ->
+    let childNames = fs.readdirSync(src);
+    // console.log(childNames);
+    for (let i = 0; i < childNames.length; i++) {
+        let childAddress = path.join(src, childNames[i]);
+        let isFile = fs.lstatSync(childAddress).isFile();
+        if (isFile) {
+            // console.log(childNames[i]);
+            let category = getCategory(childNames[i]);
+            console.log(childNames[i], "belongs to --> ", category);
+            // 4. copy / cut  files to that organized directory inside of any of category folder 
+            sendFiles(childAddress, dest, category);
+        }
+    }
+}
+function sendFiles(srcFilePath, dest, category) {
+    // 
+    let categoryPath = path.join(dest, category);
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+    let fileName = path.basename(srcFilePath);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(srcFilePath, destFilePath);
+    fs.unlinkSync(srcFilePath);
+    console.log(fileName, "copied to ", category);
+
+}
+function getCategory(name) {
+    let ext = path.extname(name);
+    ext = ext.slice(1);
+    for (let type in types) {
+        let cTypeArray = types[type];
+        for (let i = 0; i < cTypeArray.length; i++) {
+            if (ext == cTypeArray[i]) {
+                return type;
+            }
+        }
+    }
+    return "others";
+}
+
+
+
+
+// Help fn 
+function helpFn() {
+    console.log(`
+    List of All the commands:
+                 node main.js tree "directoryPath"
+                node main.js organize "directoryPath"
+                node main.js help
+                `);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
